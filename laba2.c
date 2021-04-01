04a7f1daa92e4470d51de61a102075143940ce9c
user <user@ws1013402134.pm>
2021-04-01 14:56:00 +0500

    laba3

--- laba2.c
+++ laba2.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <math.h>
-int main ()
+int main()
 {
     double x;
-    printf("Введите х {-0.3<=x<=0.3}  ");
+    printf("Введите х {0.2<=x<=0.3}  ");
     scanf("%lf", &x);
-    if ((x >= -0.3) && (x<= 0.3))
+    if ((x >= 0.2) && (x <= 0.3))
     {
-        double y=sqrt(sin(2*x))+sqrt(sin(3*x));
-        double z=pow(log(tan(y-M_PI/8)),0.25);
-        printf("y(x)=%f \n", y);
-        printf("z(x)=%f \n", z);
+        double y = sqrt(sin(2.0 * x)) + sqrt(sin(3.0 * x));
+        double z = pow(log(tan(y - M_PI / 8.0)), 0.25);
+        printf("y(x)=%lf \n", y);
+        printf("z(x)=%lf \n", z);
     }
     else
         printf("Недопустимо введенное значение \n");
