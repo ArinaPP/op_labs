5dea5c0a63e5997681ffbfd665b925f5a2c4b9aa
user <user@ws1013402134.pm>
2021-04-01 14:47:34 +0500

    laba3

---
+++ laba3.c
@@ -0,0 +1,14 @@
+// вар 5
+
+#include <stdio.h>
+#include <math.h>
+int main ()
+{
+    float x, f, h;
+    unsigned long int i, n;
+    printf("Введите h=\n");
+    scanf(%f, &h);
+    printf("x\t\tf(x)\n");
+    printf("--------------------");
+    
+}
