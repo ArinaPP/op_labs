74142877f1283244ba0a8d8e46d1fee5c4d2e2c3
user <user@ws1013402134.pm>
2021-04-15 16:02:25 +0500

    lab3.2

--- laba3.c
+++ laba3.c
@@ -2,13 +2,31 @@
 
 #include <stdio.h>
 #include <math.h>
-int main ()
+int main()
 {
-    float x, f, h;
-    unsigned long int i, n;
-    printf("Введите h=\n");
-    scanf(%f, &h);
-    printf("x\t\tf(x)\n");
-    printf("--------------------");
-    
+  float x, h, f;
+  unsigned long int i, n;
+  printf("--> h\n");
+  scanf("%f", &h);
+  printf("x f(x)\n");
+  printf("---------------------------\n");
+  n = (3.0f / h);
+  i = 0;
+  do
+  {
+    if ((x >= 0.0f) && (x <= 1.5f))
+    {
+      f = pow(2.0f , x) - 2.0f + pow(x , 2.0f);
+      printf("%f %f\n", x, f);
+    }
+    else
+    {
+      f = sqrt(x) * exp(pow(x , 2.0));
+      printf("%f %f\n", x, f);
+    }
+    x = i * h;
+    i = i + 1;
+  }
+  while (i <= n);
+  return 0;
 }
